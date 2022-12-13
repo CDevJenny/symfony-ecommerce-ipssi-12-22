05923c48c226f46577442fa870ffaf3124cdcb5d
CJenny <c.dev.jenny@gmail.com>
2022-12-13 17:45:03 +0100

    ADD crud user

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,10 +21,17 @@ class ContentController extends AbstractController
     }
 
     #[Route('/products', name: 'app_products', methods: ['GET'])]
-    public function indexProducts(ProductRepository $productRepository): Response
+    public function indexProducts(ProductRepository $productRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        $allProducts = $productRepository->findAll();
+
+        $products = $paginator->paginate(
+            $allProducts, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            9 // Nombre de résultats par page
+        );
         return $this->render('content/product/index.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $products,
         ]);
     }
 
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
-use App\Security\UserAuthenticator;
+use App\Form\UserPasswordType;
 use DateTimeImmutable;
+use App\Form\UserType;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -70,12 +74,59 @@ class UserController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-    
+
     #[Route(path: '/profile/{id}', name: 'app_profile')]
-    public function indexProfile(User $user) {
-        
+    public function indexProfile(User $user, Request $request, PaginatorInterface $paginator)
+    {
+        $userProducts = $user->getProducts()->toArray();
+
+        $products = $paginator->paginate(
+            $userProducts, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            4 // Nombre de résultats par page
+        );
+
         return $this->render('security/profile/index.html.twig', [
             'user' => $user,
+            'products' => $products
+        ]);
+    }
+
+    #[Route('/profile/{id}/update', name: 'app_user_update', methods: ['GET', 'POST'])]
+    public function updateProfile(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setUpdatedAt(new DateTimeImmutable('now'));
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_profile', ["id" => $user->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('security/profile/update.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/profile/{id}/password', name: 'app_user_password', methods: ['GET', 'POST'])]
+    public function updatePassword(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserPasswordType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setUpdatedAt(new DateTimeImmutable('now'));
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_profile', ["id" => $user->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('security/profile/password.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
 }
